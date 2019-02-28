c43eef42e8ec724a3614565cd0e946b5154c99cc
bsmmoon <bsmmoon@gmail.com>
2019-02-28 16:47:27 +0800

    haha

--- main.rb
+++ main.rb
@@ -1,55 +1,68 @@
 =begin
   TODO
-  1. If line ends with ",", it should be merged with the previous line
-  2. If there is a "class_name" token, partner should be the string that comes after
-  3. If multiple files are given, remove belongs_to that is not required
+  -[x] If line ends with ",", it should be merged with the previous line
+  -[x] If there is a "class_name" token, partner should be the string that comes after
+  -[] Multiple files support
+  -[] If multiple files are given, remove belongs_to that is not required
 =end
 
 require 'active_support/inflector'
 
 class Main
   def self.run
-    Main.new.run(Main.data(ARGV[0]).split("\n"))
+    file = Main.read_file(ARGV[0])
+    data = preprocess(file)
+    Main.new.run(data)
   end
 
   BLACKLIST = Set.new %w'ActiveSupportConcern'
+
+  def self.preprocess(file)
+    file.
+      split("\n").
+      map(&:strip).
+      join("\n").
+      gsub(",\n", ', ').
+      split("\n")
+  end
 
   def run(lines)
     klass = ''
     lines.map do |line|
       tokens = line.strip.split(' ')
-      case relation = tokens.shift
-      when 'class'
+      relation = tokens.shift
+
+      next if tokens.include? 'through:'
+
+      if %w'class module'.include? relation
         klass = "#{klass}#{tokens.shift}"
-        klass.slice!('::')
-        next
-      when 'module'
-        klass = "#{klass}#{tokens.shift}"
-        klass.slice!('::')
-        next
-      when 'belongs_to'
-        partner = tokens.shift
-      when 'has_many'
-        partner = tokens.shift.singularize
-      when 'has_one'
-        partner = tokens.shift
-      when 'include'
-        partner = tokens.shift
-      when 'extend'
-        partner = tokens.shift
-      else
+        klass = klass.gsub('::', '')
         next
       end
+
+      next unless %w'belongs_to has_one include extend has_many'.include? relation
+
+      partner = tokens.shift  
+      
+      class_name_index = tokens.index('class_name:')
+      partner = tokens[class_name_index + 1] unless class_name_index.nil?
+      
       partner = partner[1...partner.length] if partner[0] == ':'
       partner = partner[0...partner.length-1] if partner[partner.length - 1] == ','
-      partner.slice!('::')
+      
+      partner = partner.singularize if %w'has_many'.include? relation
+      
+      partner = partner.gsub('::', '')
+      partner = partner.gsub("'", '')
       partner = partner.camelize
+      
       next if BLACKLIST.include? partner
-      "#{partner} : > #{relation}"
-    end.reject(&:nil?).map{|rel| "#{klass} -- #{rel}"}.sort.each{|e| puts e}
+      
+      "#{klass} -- #{partner} : > #{relation}"
+    end.reject(&:nil?).sort.each{|e| puts e}
   end
 
-  def self.data(filename)
+  def self.read_file(filename)
     file = File.open(filename, "r")
     data = file.read
     file.close
