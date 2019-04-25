f26785a9cb57506c217595ea0fa9e6d5d7177a74
Seokmin <bsmmoon@gmail.com>
2019-04-25 18:55:48 +0800

    preparing to make go parser

--- main.rb
+++ main.rb
@@ -10,15 +10,21 @@ require 'active_support/inflector'
 
 class Main
   def self.run
-    file = Main.read_file(ARGV[0])
-    data = preprocess(file)
-    Main.new.run(data)
+    language = ARGV[0]
+    case language
+    when 'ruby'
+      include RubyParser
+    when 'go'
+    end
+    filenames = Dir["#{ARGV[1]}**/*"].reject {|fn| File.directory?(fn) }
+    puts filenames.map{|filename| Main.new.run(preprocess(read_file(filename)))}
   end
 
   BLACKLIST = Set.new %w'ActiveSupportConcern'
 
   def self.preprocess(file)
     file.
+      encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').
       split("\n").
       map(&:strip).
       join("\n").
@@ -26,47 +32,50 @@ class Main
       split("\n")
   end
 
-  def run(lines)
-    klass = ''
-    lines.map do |line|
-      tokens = line.strip.split(' ')
-      relation = tokens.shift
-
-      next if tokens.include? 'through:'
-
-      if %w'class module'.include? relation
-        klass = "#{klass}#{tokens.shift}"
-        klass = klass.gsub('::', '')
-        next
-      end
-
-      next unless %w'belongs_to has_one include extend has_many'.include? relation
-
-      partner = tokens.shift  
-      
-      class_name_index = tokens.index('class_name:')
-      partner = tokens[class_name_index + 1] unless class_name_index.nil?
-      
-      partner = partner[1...partner.length] if partner[0] == ':'
-      partner = partner[0...partner.length-1] if partner[partner.length - 1] == ','
-      
-      partner = partner.singularize if %w'has_many'.include? relation
-      
-      partner = partner.gsub('::', '')
-      partner = partner.gsub("'", '')
-      partner = partner.camelize
-      
-      next if BLACKLIST.include? partner
-      
-      "#{klass} -- #{partner} : > #{relation}"
-    end.reject(&:nil?).sort.each{|e| puts e}
-  end
-
   def self.read_file(filename)
     file = File.open(filename, "r")
     data = file.read
     file.close
     data  
+  end
+
+  module RubyParser
+    def run(lines)
+      klass = ''
+      lines.map do |line|
+        tokens = line.strip.split(' ')
+        relation = tokens.shift
+  
+        next if tokens.include? 'through:'
+  
+        if %w'class module'.include? relation
+          klass = "#{klass}#{tokens.shift}"
+          klass = klass.gsub('::', '')
+          next
+        end
+  
+        next unless %w'belongs_to has_one include extend has_many'.include? relation
+  
+        partner = tokens.shift  
+        
+        class_name_index = tokens.index('class_name:')
+        partner = tokens[class_name_index + 1] unless class_name_index.nil?
+        
+        partner = partner[1...partner.length] if partner[0] == ':'
+        partner = partner[0...partner.length-1] if partner[partner.length - 1] == ','
+        
+        # partner = partner.singularize if %w'has_many'.include? relation
+        next if %w'has_many has_one'.include? relation
+        
+        partner = partner.gsub(':', '')
+        partner = partner.gsub("'", '')
+        partner = partner.camelize
+        
+        next if BLACKLIST.include? partner
+        
+        "#{klass} -- #{partner} : > #{relation}"
+      end.reject(&:nil?).sort.each{|e| puts e}
+    end  
   end
 end
 
